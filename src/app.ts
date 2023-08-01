089228b4161da976e0074f394a87fd63f1572f45
Rafael-RD <rafaelreinertd@hotmail.com>
2023-08-01 19:42:26 -0300

    feat: redis connection

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import "express-async-errors";
 import express, { Express } from "express";
 import cors from "cors";
 
-import { loadEnv, connectDb, disconnectDB } from "@/config";
+import { loadEnv, connectDb, disconnectDB, connectRedis, disconnectRedis } from "@/config";
 
 loadEnv();
 
@@ -36,11 +36,13 @@ app
 
 export function init(): Promise<Express> {
   connectDb();
+  connectRedis();
   return Promise.resolve(app);
 }
 
 export async function close(): Promise<void> {
   await disconnectDB();
+  await disconnectRedis();
 }
 
 export default app;
