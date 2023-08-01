db93ea36cf13792397e100934114e7c68d8c09a9
Viniccius Wirz <vinimovel23@gmail.com>
2023-08-01 18:04:00 -0300

    fix: hotel listings

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -3,6 +3,7 @@ import enrollmentRepository from "@/repositories/enrollment-repository";
 import ticketRepository from "@/repositories/ticket-repository";
 import { notFoundError } from "@/errors";
 import { cannotListHotelsError } from "@/errors/cannot-list-hotels-error";
+import { formatHotels } from "@/utils/hotel-format";
 
 async function listHotels(userId: number) {
   //Tem enrollment?
@@ -13,16 +14,17 @@ async function listHotels(userId: number) {
   //Tem ticket pago isOnline false e includesHotel true
   const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
 
-  if (!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
-    throw cannotListHotelsError();
+  if (!ticket || ticket.status === "RESERVED") {
+    throw cannotListHotelsError("Payment Required");
   }
+  if(ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) throw cannotListHotelsError("Invalid ticket type");
 }
 
 async function getHotels(userId: number) {
   await listHotels(userId);
 
   const hotels = await hotelRepository.findHotels();
-  return hotels;
+  return formatHotels(hotels);
 }
 
 async function getHotelsWithRooms(userId: number, hotelId: number) {
