057fc429d08180f0634332f22497ce9eda3472c9
Viniccius Wirz <vinimovel23@gmail.com>
2023-08-01 21:14:21 -0300

    fix: hotel rooms listings

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -18,8 +18,12 @@ async function findRoomsByHotelId(hotelId: number) {
       id: hotelId,
     },
     include: {
-      Rooms: true,
-    }
+      Rooms: {
+        include: {
+          Booking: true,
+        },
+      },
+    },
   });
 }
 
