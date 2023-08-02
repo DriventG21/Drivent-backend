43b1282aa56d44fc3a7e6562349cabfc691ea79c
Rafael-RD <rafaelreinertd@hotmail.com>
2023-08-01 21:45:03 -0300

    feat: redis startup

--- src/utils/redis-startup.ts
+++ src/utils/redis-startup.ts
@@ -1,11 +1,24 @@
 import { redis } from "@/config";
 import hotelRepository from "@/repositories/hotel-repository";
+import eventRepository from "@/repositories/event-repository";
 
 export async function redisStartup() {
     await redisHotelsStartup();
+    await redisEventsStartup();
 }
 
 async function redisHotelsStartup() {
     const hotels = await hotelRepository.findHotels();
-    console.log(hotels);
+    if (hotels.length === 0) return;
+    hotels.forEach(e => {
+        redis.set(`hotel:${e.id}`, JSON.stringify(e));
+    })
+}
+
+async function redisEventsStartup() {
+    const events = await eventRepository.findAll();
+    if (events.length === 0) return;
+    events.forEach(e => {
+        redis.set(`event:${e.id}`, JSON.stringify(e));
+    })
 }
